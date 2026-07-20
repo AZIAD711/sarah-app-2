5fe52b69303a8218e598e57e80dc15549d8590bb
zeyad <aziad711@gmail.com>
2026-07-20 19:21:16 +0300

    ✅ ADDED ALL FUNCTION IN FAILED  RESPONSE

---
+++ src/common/response/error.js
@@ -0,0 +1,58 @@
+// GOLBAL FAIL RESPONSE
+export const failResponse = ({ response, message = "failed",  statusCode = 400 }) => {
+    return response.status(statusCode).json({
+        success: false,
+        message,
+    });
+}
+// DATA IS NOT FOUND RESPONSE 
+export const dataNotFoundResponse = ({
+    response,
+    message = "data",
+
+})=>{
+    return failResponse({
+        response,
+        message: `${message} is not found successfully`,
+    })
+
+}
+// AUTHORIZED RESPONSE 
+export const authorizedResponse = ({
+    response,
+    message = "authorized",
+
+})=>{
+    return failResponse({
+        response,
+        message: `${message} Access `,
+
+        statusCode: 404
+    })
+
+}
+// BAD REQUEST RESPONSE
+export const badRequestResponse = ({
+    response,
+    message = "bad request",
+
+}) => {
+    return failResponse({
+        response,
+        message,
+
+        statusCode:400
+    });
+};
+// BAD REQUEST RESPONSE
+export const internalServerResponse = ({
+    response,
+    message = "internal server",
+
+}) => {
+    return failResponse({
+        response,
+        message,
+        statusCode:500
+    });
+};
