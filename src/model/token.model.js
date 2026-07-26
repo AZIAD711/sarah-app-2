eb743d1ac6d9b7e4b0ac33d6f35a75f40c460062
zeyad <aziad711@gmail.com>
2026-07-26 07:35:12 +0300

    ✅ NEW MODEL ADDED :  TOKEN

---
+++ src/model/token.model.js
@@ -0,0 +1,39 @@
+import { model, Schema, Types } from "mongoose";
+import { MessageFlag } from "../common/enum/message-flag.js";
+import { ref } from "node:process";
+import { type } from "node:os";
+// TOKEN SCHEMA
+const tokenSchema = new Schema(
+    {
+        // USER ID 
+        userId: {
+            type: Types.ObjectId,
+            trim: true,
+            required: true,
+            ref: "User"
+        },
+        // JTI 
+        jit: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        // EXPIRE IN
+        expireIn: {
+            type: Date,
+            required: true,
+        },
+    },
+    {
+        strict: true,
+        strictQuery: true,
+        timestamps: true,
+        collection: "token_data",
+        versionKey: "version",
+    }
+);
+tokenSchema.index("expireIn", { expireAfterSeconds: 0 })
+
+const tokenModel = model("Token", tokenSchema);
+
+export default tokenModel;
