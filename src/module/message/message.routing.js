43b603f780323bcfb757bd6e20253ad5a9f3e8ef
zeyad <aziad711@gmail.com>
2026-08-01 00:26:57 +0300

    ✅ NEW UPDATE

--- src/module/message/message.routing.js
+++ src/module/message/message.routing.js
@@ -1,16 +1,75 @@
+import express from "express";
+import {
+    sendMessageController,
+    getMyMessagesController,
+    listAllMessagesController,
+    deleteMessageController,
+    deleteMessageByAdminController,
+    replyMessageController
+} from "./message.controller.js";
 
-import {optionalAuthentication,authorization} from "../../common/middleware/optional-auth.middleware.js"
-import express from "express"
-import { UserRole } from "../../common/enum/user-role.js"
-import { sendMessageController ,getMyMessagesController, listAllMessagesController, deleteMessageController, deleteMessageByAdminController, replyMessageController} from "./message.controller.js"
-import { authentication } from "../../common/middleware/auth.middleware.js"
-import { schemaValidate } from "../../common/middleware/valdiate.middelware.js"
-import { sendMessageSchema,deleteMessageSchema,replyMessageSchema,updateMessageFlagSchema } from "./message.valdiate.js"
-const messageRouter = express.Router()
-messageRouter.post("/send/:reciverId",schemaValidate(sendMessageSchema),optionalAuthentication(),authorization(UserRole.USER),sendMessageController)
-messageRouter.get("/get/me",optionalAuthentication(),authorization(UserRole.USER),getMyMessagesController)
-messageRouter.get("/list",optionalAuthentication(),authorization(UserRole.ADMIN),listAllMessagesController)
-messageRouter.post("/reply/:messageId",schemaValidate(replyMessageSchema),authentication(),authorization(UserRole.USER),replyMessageController)
-messageRouter.delete("/delete/:messageId",optionalAuthentication(),authorization(UserRole.USER),deleteMessageController)
-messageRouter.delete("/kill/:messageId",optionalAuthentication(),authorization(UserRole.ADMIN),deleteMessageByAdminController)
-export default messageRouter
+import {
+    sendMessageSchema,
+    replyMessageSchema,
+    deleteMessageSchema
+} from "./message.valdiate.js";
+
+import { schemaValidate } from "../../common/middleware/valdiate.middelware.js";
+import { authentication,authorization } from "../../common/middleware/auth.middleware.js";;
+import { UserRole } from "../../common/enum/user-role.js";
+
+const messageRouter = express.Router();
+
+// Send Message
+messageRouter.post(
+    "/send/:reciverId",
+    authentication(),
+    authorization(UserRole.USER),
+    schemaValidate(sendMessageSchema),
+    sendMessageController
+);
+
+// Get My Messages
+messageRouter.get(
+    "/get/me",
+    authentication(),
+    authorization(UserRole.USER),
+    getMyMessagesController
+);
+
+// List All Messages
+messageRouter.get(
+    "/list",
+    authentication(),
+    authorization(UserRole.ADMIN),
+    listAllMessagesController
+);
+
+// Reply Message
+messageRouter.post(
+    "/reply/:messageId",
+    authentication(),
+    authorization(UserRole.USER),
+    schemaValidate(replyMessageSchema),
+    replyMessageController
+);
+
+// Delete My Message
+messageRouter.delete(
+    "/delete/:messageId",
+    authentication(),
+    authorization(UserRole.USER),
+    schemaValidate(deleteMessageSchema),
+    deleteMessageController
+);
+
+// Delete Any Message (Admin)
+messageRouter.delete(
+    "/kill/:messageId",
+    authentication(),
+    authorization(UserRole.ADMIN),
+    schemaValidate(deleteMessageSchema),
+    deleteMessageByAdminController
+);
+
+export default messageRouter;
