194d16749f780a28b4cc846caed9b1d5844af071
zeyad <aziad711@gmail.com>
2026-08-01 09:36:41 +0300

    ✅ NEW ITEM ADDED : CORS

--- app.controller.js
+++ app.controller.js
@@ -1,17 +1,34 @@
 import express from "express"
 import dotenv, { config } from "dotenv"
-import {databaseConnection} from "./src/database/db-connection.js"
+import { databaseConnection } from "./src/database/db-connection.js"
 import userRouter from "./src/module/auth/auth.routing.js"
 import messageRouter from "./src/module/message/message.routing.js"
 import { redisConnection } from "./src/database/redis-connection.js"
-export const app =()=>{
+import cor from "cor"
+export const app = () => {
+    const PORT = process.env.SERVER_PORT
+    const whishList = [`http://localhost:${PORT}`, `http://localhost:6000`]
+    var corsOptions = {
+        origin: function (origin, callback) {
+            if (whishList.indexOf(origin) !== -1 || !origin) {
+                callback(null, true)
+            }
+            else {
+                callback(new Error("Not allowed by CORS"))
+            }
+        },
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
+    }
     dotenv.config();
-     databaseConnection()
-     redisConnection()
+    databaseConnection()
+    redisConnection()
     const router = express()
+    router.use(cor(corsOptions))
     router.use(express.json())
-    router.use("/auth",userRouter)
-    router.use("/message",messageRouter)
+    router.use("/auth", userRouter)
+    router.use("/message", messageRouter)
     return router
 }
 export default app
